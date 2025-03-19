c72bbb7c30ff415e6f1f1543881bb12a656650b8
ytgoh2021 <112225704+ytgoh2021@users.noreply.github.com>
2025-03-19 17:48:34 +0800

    added lottie files

---
+++ app/components/steam_lottie.js
@@ -0,0 +1,11 @@
+"use client"
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+
+export default function SteamLottie() {
+    return <DotLottieReact
+    src="https://lottie.host/f13f2818-a976-4e6a-8a99-1877f90a96f9/EAyLfrWDWd.lottie"
+    loop
+    autoplay
+    className='w-auto h-auto border-4 border-amber-400'
+  />
+}
---
+++ app/home/page.js
@@ -0,0 +1,9 @@
+import SteamLottie from "../components/steam_lottie";
+
+export default function Home() {
+  return (
+    <div className="w-full h-screen justify-center items-center flex border-4 border-amber-600">
+      <SteamLottie></SteamLottie>
+    </div>
+  );
+}
