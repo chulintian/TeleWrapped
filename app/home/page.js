14bcbf1a883c12da3b8ddfbcde25ec2bab8dc424
ytgoh2021 <112225704+ytgoh2021@users.noreply.github.com>
2025-03-20 23:56:33 +0800

    home page

--- app/home/page.js
+++ app/home/page.js
@@ -1,9 +1,28 @@
+import Image from "next/image";
 import SteamLottie from "../components/steam_lottie";
 
 export default function Home() {
   return (
-    <div className="w-full h-screen justify-center items-center flex border-4 border-amber-600">
-      <SteamLottie></SteamLottie>
+    <div className="w-full h-screen justify-center items-center flex">
+      <div className="flex flex-col items-center w-full">
+        <div className="flex flex-row">
+          <div className="flex-1/5"></div>
+          <div className="flex-4/5">
+            <SteamLottie></SteamLottie>
+          </div>
+        </div>
+        <div className="justify-center items-center">
+          <Image
+            src="/hotpot/pot.png"
+            width={500}
+            height={300}
+            layout="intrinsic"
+            objectFit="contain"
+            alt="Image of hotpot"
+            className="-mt-25"
+          />
+        </div>
+      </div>
     </div>
   );
 }
