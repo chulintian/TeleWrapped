a4ba8b9a20494b34976a0daa002bae6ee58b391b
ytgoh2021 <112225704+ytgoh2021@users.noreply.github.com>
2025-03-25 17:15:22 +0800

    Increased size of steam lottie & added audio

---
+++ app/components/home/food_sizzle.js
@@ -0,0 +1,7 @@
+export default function FoodSizzleAudio() {
+  return (
+    <audio autoPlay loop>
+      <source src="/sound_effects/food_sizzle1.mp3" type="audio/mpeg" />
+    </audio>
+  );
+}
--- app/home/page.js
+++ app/home/page.js
@@ -1,26 +1,21 @@
-import Image from "next/image";
-import SteamLottie from "../components/steam_lottie";
+import FoodSizzleAudio from "../components/home/food_sizzle";
+import SteamLottie from "../components/home/steam_lottie";
 
 export default function Home() {
   return (
     <div className="w-full h-screen justify-center items-center flex">
-      <div className="flex flex-col items-center w-full">
-        <div className="flex flex-row">
-          <div className="flex-1/5"></div>
-          <div className="flex-4/5">
+      <FoodSizzleAudio></FoodSizzleAudio>
+      <div className="flex flex-col items-center max-w-screen-lg w-full">
+        <div className="flex flex-row justify-end w-full ">
+          <div className="w-4/5">
             <SteamLottie></SteamLottie>
           </div>
         </div>
-        <div className="justify-center items-center">
-          <Image
+        <div className="flex flex-row justify-center w-full ">
+          <img
+            className="w-5/6 -mt-25 sm:-mt-40 md:-mt-48 lg:-mt-60 "
             src="/hotpot/pot.png"
-            width={500}
-            height={300}
-            layout="intrinsic"
-            objectFit="contain"
-            alt="Image of hotpot"
-            className="-mt-25"
-          />
+          ></img>
         </div>
       </div>
     </div>
